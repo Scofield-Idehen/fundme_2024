b3c950a3ff6639c8317d96c648700b2cc43c765c
scofield <scofields109@gmail.com>
2024-11-15 20:35:32 +0100

    foundry update

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,10 +2,15 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
 
+    uint8 public constant DECIMAL = 8;
+    int public constant INITIAL = 2000e8;
+
     NetworkConfig public activeNetwork;
+
 
      struct NetworkConfig{
         address priceFeed;
@@ -14,7 +19,9 @@ contract HelperConfig is Script {
     constructor(){
         if (block.chainid == 11155111) {
             activeNetwork = GetSapolaConfig();
-        } else {
+        }else if (block.chainid == 1){
+            activeNetwork = GetETHConfig();   
+        }else {
             activeNetwork = GetAnvilConfig();
         }
     }
@@ -29,7 +36,30 @@ contract HelperConfig is Script {
         return sapolaConfig;
     }
 
-    function GetAnvilConfig() pure public returns (NetworkConfig memory){
+    function GetETHConfig() pure public returns (NetworkConfig memory){
+        //this checks if  we have created a contract 
+        //if yes it wont create again
+        NetworkConfig memory EthConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return EthConfig;
+    }
+
+    function GetAnvilConfig() public returns (NetworkConfig memory){
+        if (activeNetwork.priceFeed != address(0)){
+            return activeNetwork;
+        }
+        
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMAL, INITIAL);
+
+        vm.stopBroadcast();
+
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return anvilConfig;
         
     }
 }
